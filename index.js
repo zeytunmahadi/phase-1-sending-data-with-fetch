438aefd96656a6ace622afac6c834b35c5fe2af5
zeitun <zeytunmahadi718@gmail.com>
2024-06-06 15:20:05 +0300

    post and delete data from the server

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// updating the dom
 function addmovie(movie){
     const row = document.getElementById("card")
     const div = document.createElement("div")
@@ -7,14 +9,32 @@ function addmovie(movie){
     <div class="card-body">
       <h5 class="card-title">${movie.Title}</h5>
       <p class="card-text">${movie.Plot}</p>
-      <a href="#" class="btn btn-danger">delete</a>
+      <button><a href="#" class="btn btn-danger">delete</a></button>
     </div>
   </div>`
   row.appendChild(div)
-} 
+ 
+div.querySelector('button').addEventListener('click' ,function(){
+  div.remove()
+  deleteData(movie.id);
+})
+function deleteData(){
+  fetch(`http://localhost:3000/movies/${movie.id}`,{
+    method: 'DELETE',
+    header: {
+      'content-Type':'application/JSON'
+    },
+
+  })
+}
+        
+
+  
+}
 
 
 
+//fetching the data from db.json
 
 function getmovies(){
     fetch("http://localhost:3000/movies")
@@ -24,8 +44,37 @@ function getmovies(){
 
 }
 
+//loading other content bfre js
 
 document.addEventListener('DOMContentLoaded',function(){
     getmovies()
 
 })
+
+//posting a new data
+document.querySelector('form').addEventListener("submit",function(e){
+  createmovie()
+  e.preventDefault()
+
+})
+
+function createmovie(){
+  let title = document.getElementById("title")
+  let plot = document.getElementById('plot')
+  let poster =document.getElementById("poster")
+
+const object ={
+  Title:title.value,
+  Plot:plot.value,
+  Poster:poster.value
+
+}
+fetch('http://localhost:3000/movies',{
+  method: 'POST',
+  header: {
+    'content-Type':'application/JSON'
+  },
+  body:JSON.stringify(object)
+})
+}
+
